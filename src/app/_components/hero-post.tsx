0d6b2f1750b88f445f03c969af72afcf07a9f210
sn4kstml <151861051+sn4kstml@users.noreply.github.com>
2024-09-26 14:38:04 +0300

    fix

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -27,6 +27,22 @@ export function HeroPost({
       <div className="mb-8 md:mb-16">
         <ArticleForm /> {/* Заменяем изображение на компонент ArticleForm */}
       </div>
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+        <div>
+          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
+            <Link href={`/posts/${slug}`} className="hover:underline">
+              {title}
+            </Link>
+          </h3>
+          <div className="mb-4 md:mb-0 text-lg">
+            <DateFormatter dateString={date} />
+          </div>
+        </div>
+        <div>
+          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          <Avatar name={author.name} picture={author.picture} />
+        </div>
+      </div>
     </section>
   );
 }
--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -3,7 +3,6 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
-import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "./globals.css";
 
